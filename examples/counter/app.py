3c58312a1682a17235024fb305a8ca5545485845
Poga Po <poga.bahamut@gmail.com>
2015-09-21 20:09:52 +0800

    fix example

--- examples/counter/app.py
+++ examples/counter/app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return jsonify(redis.get('hits'))
+    return jsonify(result=redis.get('hits'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
