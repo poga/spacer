e4b7869660c1135a46ca3dd70a312e16272dc67e
Poga Po <poga.bahamut@gmail.com>
2015-09-21 18:52:32 +0800

    add proxy

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"net/url"
 	"os"
+	"regexp"
 	"strings"
-
-	"gopkg.in/yaml.v2"
 )
 
 func main() {
@@ -28,90 +28,84 @@ func main() {
 		}
 
 		// git clone path
-		dir := strings.Split(l, "/")
-		if len(dir) < 2 {
-			continue
-		}
 		/*
-			url := "git@github.com:" + dir[0] + "/" + dir[1] + ".git"
-			os.Mkdir("services", 0777)
-			to := fmt.Sprintf("services/%s", dir[1])
-			output, err := exec.Command("git", "clone", url, to).CombinedOutput()
-			if err != nil {
-				log.Fatal(err)
+			dir := strings.Split(l, "/")
+			if len(dir) < 2 {
+				continue
 			}
-			fmt.Println(string(output))
+				url := "git@github.com:" + dir[0] + "/" + dir[1] + ".git"
+				os.Mkdir("services", 0777)
+				to := fmt.Sprintf("services/%s", dir[1])
+				output, err := exec.Command("git", "clone", url, to).CombinedOutput()
+				if err != nil {
+					log.Panic(err)
+				}
+				fmt.Println(string(output))
 		*/
-		s := Service{l, dir[1]}
+		s := NewService(l)
 		services = append(services, s)
 
 		// docker-compose build && docker-compose up
-		/*
-			dcb, err := s.Build()
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println(string(dcb))
+		dcb, err := s.Build()
+		if err != nil {
+			log.Panic(err)
+		}
+		fmt.Println(string(dcb))
 
-			s.Start()
+		s.Start()
+		/*
 
 			time.Sleep(3 * time.Second)
 
 			dcs, err := s.Stop()
 			if err != nil {
-				log.Fatal(err)
+				log.Panic(err)
 			}
 			fmt.Println(string(dcs))
 
 		*/
-		// loop through docker-compose.yml and look for exposed ip, save it
-		ymlBytes, err := ioutil.ReadFile(s.ConfigPath())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		m := make(map[interface{}]interface{})
-		err = yaml.Unmarshal(ymlBytes, &m)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("%v\n", m)
-
-		// if ports == "5000:5000", take the former port
-		// if ports == "5000", ask "docker-compose port SERVICE_NAME 5000" to know the exposed port
-		for serviceName, conf := range m {
-			if ports, ok := conf.(map[interface{}]interface{})["ports"]; ok {
-				// TODO handle multiple exposed ports
-				portValue := ports.([]interface{})[0].(string)
-				var innerPort string
-
-				// TODO handle 127.0.0.1:8001:8001 style config
-				if len(strings.Split(portValue, ":")) == 2 {
-					innerPort = strings.Split(portValue, ":")[1]
-				} else {
-					innerPort = portValue
-				}
-				output, err := s.GetExposedURL(serviceName.(string), innerPort)
-				if err != nil {
-					log.Fatal(err)
-				}
-				exposedURLs = append(exposedURLs, output)
-
-			}
-		}
-
 	}
 
 	fmt.Printf("%v\n", services)
 	fmt.Printf("%v\n", exposedURLs)
 
-	// setup a proxy to all services
-	proxy := httputil.NewSingleHostReverseProxy(nil)
-	proxy.Director = func(r *http.Request) {
-		for _, s := range services {
-			if strings.HasPrefix(r.URL.Path, s.Name) {
-				// TODO change URL's host to container url
-			}
+	// setup a proxy for each service
+	for _, s := range services {
+		// TODO not just web
+		prefix := "/" + s.Name
+		fmt.Printf("Proxying %s to %s\n", prefix, s.ExposedURLs["web"])
+		proxy := httputil.NewSingleHostReverseProxy(s.ExposedURLs["web"])
+		proxy.Director = direct(prefix, s.ExposedURLs["web"])
+		http.HandleFunc(prefix, proxy.ServeHTTP)
+	}
+
+	http.ListenAndServe(":9064", nil)
+}
+
+func direct(prefix string, target *url.URL) func(req *http.Request) {
+	regex := regexp.MustCompile(`^` + prefix)
+	return func(req *http.Request) {
+		targetQuery := target.RawQuery
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = regex.ReplaceAllString(singleJoiningSlash(target.Path, req.URL.Path), "")
+		fmt.Printf("redirected: %s %s %s\n", req.URL.Scheme, req.URL.Host, req.URL.Path)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
 		}
 	}
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
 }
--- service.go
+++ service.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
 	"os/exec"
+	"path/filepath"
 	"strings"
+	"time"
+
+	"gopkg.in/yaml.v2"
 )
 
 type Service struct {
-	Name string
-	Root string
+	Name        string
+	Root        string
+	ExposedURLs map[string]*url.URL
+}
+
+func NewService(path string) Service {
+	s := Service{
+		Name:        filepath.Base(path),
+		Root:        path,
+		ExposedURLs: make(map[string]*url.URL),
+	}
+
+	return s
 }
 
 func (s Service) ConfigPath() string {
@@ -22,7 +40,51 @@ func (s Service) Build() ([]byte, error) {
 
 func (s Service) Start() error {
 	fmt.Println("Starting", s.ConfigPath())
-	return exec.Command("docker-compose", "-f", s.ConfigPath(), "up").Start()
+	err := exec.Command("docker-compose", "-f", s.ConfigPath(), "up").Start()
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(5 * time.Second)
+
+	// loop through docker-compose.yml and look for exposed ip, save it
+	ymlBytes, err := ioutil.ReadFile(s.ConfigPath())
+	if err != nil {
+		log.Panic(err)
+	}
+	m := make(map[string]interface{})
+	err = yaml.Unmarshal(ymlBytes, &m)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Printf("%v\n", m)
+
+	for serviceName, conf := range m {
+		if ports, ok := conf.(map[interface{}]interface{})["ports"]; ok {
+			// TODO handle multiple exposed ports
+			portValue := ports.([]interface{})[0].(string)
+			var innerPort string
+
+			// TODO handle 127.0.0.1:8001:8001 style config
+			if len(strings.Split(portValue, ":")) == 2 {
+				innerPort = strings.Split(portValue, ":")[1]
+			} else {
+				innerPort = portValue
+			}
+			output, err := s.getExposedURL(serviceName, innerPort)
+			if err != nil {
+				log.Panic(err)
+			}
+			u, err := url.Parse("http://" + output)
+			if err != nil {
+				log.Panic(err)
+			}
+			s.ExposedURLs[serviceName] = u
+		}
+	}
+
+	exec.Command("docker-compose", "-f", s.ConfigPath(), "up").Start()
+	return nil
 }
 
 func (s Service) Stop() ([]byte, error) {
@@ -30,7 +92,8 @@ func (s Service) Stop() ([]byte, error) {
 	return exec.Command("docker-compose", "-f", s.ConfigPath(), "stop").CombinedOutput()
 }
 
-func (s Service) GetExposedURL(serviceName string, port string) (string, error) {
+func (s Service) getExposedURL(serviceName string, port string) (string, error) {
+	fmt.Println("getExposedURL", serviceName, port, s.ConfigPath())
 	output, err := exec.Command("docker-compose", "-f", s.ConfigPath(), "port", serviceName, port).CombinedOutput()
 	if err != nil {
 		return "", err
