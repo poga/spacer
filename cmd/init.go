00cc0ba980803c812555e66d8f8ed5746551ed46
Poga Po <poga.bahamut@gmail.com>
2017-12-26 02:41:34 +0800

    init nginx template

--- cmd/init.go
+++ cmd/init.go
@@ -1,15 +1,12 @@
 package cmd
 
 import (
-	"fmt"
-	"io"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-
-	"github.com/pkg/errors"
+	spacer "github.com/poga/spacer/pkg"
 	"github.com/spf13/cobra"
 )
+
+var targetDir string
+var shallow bool
 
 var initCmd = &cobra.Command{
 	Use:   "init",
@@ -17,101 +14,18 @@ var initCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		// TODO:
 		// 1. write nginx configs to target directory
+		if !shallow {
+			spacer.RestoreAssets(targetDir, "nginx")
+			return
+		}
 		// TODO: shallow - symlink to nginx configs in the repo
-		copyDir()
 		// 2. add spacer.yml
 		// 3. hello world function
-
 	},
 }
 
 func init() {
+	initCmd.Flags().StringVarP(&targetDir, "target", "t", "", "Target directory to init a new spacer project")
+	initCmd.Flags().BoolVarP(&shallow, "shallow", "s", false, "Create symlink instead of copying project template to target directory. Useful for development")
 	RootCmd.AddCommand(initCmd)
-}
-
-func copyDir(from string, to string) error {
-	from = filepath.Clean(from)
-	to = filepath.Clean(to)
-
-	st, err := os.Stat(from)
-	if err != nil {
-		return err
-	}
-	if !st.IsDir() {
-		return errors.Wrap(err, fmt.Sprintf("%s is not a directory", from))
-	}
-
-	_, err = os.Stat(to)
-	if err != nil && !os.IsNotExist(err) {
-		return err
-	}
-	if err == nil {
-		return errors.Wrap(err, fmt.Sprintf("%s already exists", to))
-	}
-
-	err = os.MkdirAll(to, st.Mode())
-	if err != nil {
-		return err
-	}
-
-	children, err := ioutil.ReadDir(from)
-	if err != nil {
-		return err
-	}
-	for _, c := range children {
-		fp := filepath.Join(from, c.Name())
-		tp := filepath.Join(to, c.Name())
-		if c.IsDir() {
-			err = copyDir(fp, tp)
-			if err != nil {
-				return errors.Wrap(err, fmt.Sprintf("Unable to copy dir %s", fp))
-			}
-		} else {
-			if c.Mode()&os.ModeSymlink != 0 {
-				continue
-			}
-
-			err = copyFile(fp, tp)
-			if err != nil {
-				return errors.Wrap(err, fmt.Sprintf("Unable to copy file %s", fp))
-			}
-		}
-	}
-
-	return nil
-}
-
-func copyFile(from string, to string) error {
-	in, err := os.Open(from)
-	if err != nil {
-		return err
-	}
-	defer in.Close()
-
-	out, err := os.Create(to)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, in)
-	if err != nil {
-		return err
-	}
-
-	err = out.Sync()
-	if err != nil {
-		return err
-	}
-
-	si, err := os.Stat(from)
-	if err != nil {
-		return err
-	}
-	err = os.Chmod(to, si.Mode())
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
