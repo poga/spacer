5c32ece85f665ddf867b7568dbe45b26cc408117
Poga Po <poga.bahamut@gmail.com>
2017-12-28 19:51:40 +0800

    add route support

---
+++ app/routes.lua
@@ -0,0 +1,5 @@
+local _R = {
+    {"GET", "/say_hello", "hello"}
+}
+
+return _R
--- app/spacer/run.lua
+++ app/spacer/run.lua
@@ -1,18 +1,35 @@
 local json = require "cjson"
+local routes = require "routes"
 
 ngx.req.read_body()
 
--- TODO: not safe at all
 local path = ngx.var.uri
-local ok, func = pcall(require, path)
+local method = ngx.var.request_method
 
+local module = nil
+
+for i, route in ipairs(routes) do
+    if route[1] == method and route[2] == path then
+        module = route[3]
+    end
+end
+
+if module == nil then
+    ngx.status = ngx.HTTP_NOT_FOUND
+    ngx.say(json.encode({["error"] = "not found"}))
+    return ngx.exit(ngx.HTTP_OK)
+end
+
+local ok, func = pcall(require, module)
 if not ok then
+    -- `func` will be the error message if error occured
     ngx.log(ngx.ERR, func)
     if string.find(func, "not found") then
         ngx.status = ngx.HTTP_NOT_FOUND
     else
         ngx.status = ngx.ERROR
     end
+    -- TODO: don't return error message in production
     ngx.say(json.encode({["error"] = func}))
     return ngx.exit(ngx.HTTP_OK)
 end
