65da43a4ddb93720e8a6bc1ca1a4d24581828abb
Poga Po <poga.bahamut@gmail.com>
2017-12-28 19:51:53 +0800

    don't show previous log when starting up

--- bin/start.sh
+++ bin/start.sh
@@ -3,8 +3,8 @@ trap "kill 0" EXIT
 
 openresty -p `pwd`/ -c nginx.conf &
 sleep 1
-tail -f logs/access.log &
+tail -0 -f logs/access.log &
 sleep 1
-tail -f logs/error.log &
+tail -0 -f logs/error.log &
 
 wait
