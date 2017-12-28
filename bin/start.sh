48addae86d3fe638f08aa94cb87d144691520870
Poga Po <poga.bahamut@gmail.com>
2017-12-28 20:22:02 +0800

    colorize error log

--- bin/start.sh
+++ bin/start.sh
@@ -4,7 +4,8 @@ trap "kill 0" EXIT
 openresty -p `pwd`/ -c nginx.conf &
 sleep 1
 tail -0 -f logs/access.log &
-sleep 1
-tail -0 -f logs/error.log &
+tail -0 -f logs/error.log | awk '
+  /[error]/ {print "\033[31m" $0 "\033[39m"}
+' &
 
 wait
