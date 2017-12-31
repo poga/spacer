17e428d46f4fc5a8e6f58bd6d1d2d3592326035c
Poga Po <poga.bahamut@gmail.com>
2018-01-01 01:11:52 +0800

    set log level with LOG_LEVEL env var

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
-	"log"
+	"os"
+	"strings"
 
 	"github.com/poga/spacer/cmd"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func init() {
+	switch strings.ToLower(os.Getenv("LOG_LEVEL")) {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	default:
 	}
 }
