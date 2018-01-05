6126ca850299900a069f9442e6d5dc3b100718f9
Poga Po <poga.bahamut@gmail.com>
2018-01-05 21:50:17 +0800

    protect internal service with SPACER_INTERNAL_TOKEN

--- app/spacer/run.lua
+++ app/spacer/run.lua
@@ -1,6 +1,8 @@
 local json = require "cjson"
 local router = require "router"
-local env = os.getenv('SPACER_ENV')
+
+local ENV = os.getenv('SPACER_ENV')
+local INTERNAL_TOKEN = os.getenv('SPACER_INTERNAL_TOKEN')
 
 local reject = function (status, body)
     ngx.status = status
@@ -10,7 +12,14 @@ end
 
 ngx.req.read_body()
 
-local module = router(ngx.var.request_method, ngx.var.uri)
+local module = nil
+
+-- check if this is a request from spacer within
+if ngx.req.get_uri_args()["spacer_internal_token"] == INTERNAL_TOKEN then
+    module = ngx.var.uri
+else
+    module = router(ngx.var.request_method, ngx.var.uri)
+end
 
 if module == nil then return reject(404, {["error"] = "not found"}) end
 
@@ -20,11 +29,11 @@ if not ok then
     ngx.log(ngx.ERR, func)
     local status = nil
     if string.find(func, "not found") then
-        ngx.status = 404
+        status = 404
     else
-        ngx.status = 500
+        status = 500
     end
-    if env == 'production' then
+    if ENV == 'production' then
         func = 'Internal Server Error'
     end
 
@@ -55,7 +64,7 @@ if not ok then
         return reject(400, {["error"] = ret.err})
     else -- unknown exception
         ngx.log(ngx.ERR, ret)
-        if env == 'production' then
+        if ENV == 'production' then
             ret = 'We\'re sorry, something went wrong'
         end
         return reject(500, {["error"] = ret})
