de07254a0eacf1c9122fb3722e7d664a16daca72
Poga Po <poga.bahamut@gmail.com>
2018-02-25 19:10:05 +0800

    fix production SPACER_ENV

--- bin/prod.sh
+++ bin/prod.sh
@@ -2,7 +2,7 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-openresty -p `pwd`/ -c `pwd`/config/nginx.production.conf &
+SPACER_ENV=production openresty -p `pwd`/ -c `pwd`/config/nginx.production.conf &
 LOG_LEVEL=error spacer start . --env production &
 
 wait
